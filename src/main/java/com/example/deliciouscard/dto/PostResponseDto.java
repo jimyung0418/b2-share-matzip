97cd935d006b416c9528b09d41d15af666bcdaef
이승준 <lsj135779@gmail.com>
2023-11-24 21:37:32 +0900

    게시글 조회시 정보 추가

--- src/main/java/com/example/deliciouscard/dto/PostResponseDto.java
+++ src/main/java/com/example/deliciouscard/dto/PostResponseDto.java
@@ -11,11 +11,17 @@ public class PostResponseDto extends CommonResponseDto{
     private String content;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
+    private String nickName;
+    private String restaurantName;
+    private String city;
 
     public PostResponseDto(Post post) {
         this.title = post.getTitle();
         this.content = post.getContent();
         this.createdAt = post.getCreatedAt();
         this.modifiedAt = post.getModifiedAt();
+        this.nickName = post.getUser().getNickname();
+        this.restaurantName = post.getRestaurant().getRestaurantName();
+        this.city = post.getRestaurant().getCity();
     }
 }
